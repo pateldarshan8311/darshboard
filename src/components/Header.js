124fd29c993d8cf2b1209e246e7014f3e1035b34
Darshboard <darsanpatel8311@gmail.com>
2026-02-13 01:11:24 +0530

    resposnvei inprogress

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,6 @@ import ImageComponent from './ImageComponent';
 import '../css/Common.css';
 import '../css/Header.css';
 
-
-
 const Header = ({ menuItems = [], loading, logo, favicon }) => {
   const location = useLocation();
 
@@ -22,6 +20,22 @@ const Header = ({ menuItems = [], loading, logo, favicon }) => {
     }
   }, [favicon]);
 
+  // Hamburger toggle fix
+  useEffect(() => {
+    const toggle = document.getElementById('toggle');
+    if (!toggle) return;
+
+    const handleClick = () => {
+      toggle.classList.toggle('active');
+    };
+
+    toggle.addEventListener('click', handleClick);
+
+    return () => {
+      toggle.removeEventListener('click', handleClick);
+    };
+  }, []);
+
   return (
     <header className="site_header">
       <div className="container">
@@ -30,14 +44,22 @@ const Header = ({ menuItems = [], loading, logo, favicon }) => {
           {/* Logo */}
           <div className="d_logo">
             <Link to="/">
-              {logo ? <img src={logo} alt="Site Logo" /> : <p>Loading Logo...</p>}
+              {logo ? <img src={logo} alt="Site Logo" /> : null}
             </Link>
           </div>
 
+          {/* Hamburger */}
+          <div id="toggle" className="d_hamburger_menu">
+            <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none">
+              <path fillRule="evenodd" clipRule="evenodd"
+                d="M4 5C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H4ZM7 12C7 11.4477 7.44772 11 8 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H8C7.44772 13 7 12.5523 7 12ZM13 18C13 17.4477 13.4477 17 14 17H20C20.5523 17 21 17.4477 21 18C21 18.5523 20.5523 19 20 19H14C13.4477 19 13 18.5523 13 18Z"
+                fill="#fff"
+              />
+            </svg>
+          </div>
+
           {/* Menu */}
-          {loading ? (
-            <p>⏳ Loading menu...</p>
-          ) : menuItems.length > 0 ? (
+          {loading ? null : menuItems.length > 0 ? (
             <ul className="d_main_menu">
               {menuItems.map((item, idx) => {
                 const linkPath = item.url === '/about-me/' ? '/' : item.url;
@@ -50,7 +72,7 @@ const Header = ({ menuItems = [], loading, logo, favicon }) => {
                     <Link to={linkPath}>
                       {item.icon && (
                         <span
-                          className="menu_icon "
+                          className="menu_icon"
                           dangerouslySetInnerHTML={{
                             __html: `<img src="${item.icon}" alt="icon" />`,
                           }}
@@ -62,9 +84,7 @@ const Header = ({ menuItems = [], loading, logo, favicon }) => {
                 );
               })}
             </ul>
-          ) : (
-            <p>⚠ No menu items found.</p>
-          )}
+          ) : null}
 
           {/* Email Section */}
           <div className="d_mail">
